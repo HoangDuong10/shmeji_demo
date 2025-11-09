d4ed0d4e5d0482192d13dc6c190230b3f1fd1369
Hoang Xuan Duong <hoangxuanduong12a3dqh@gmail.com>
2025-11-09 20:27:31 +0700

    oke

--- app/src/main/java/com/example/demoshemij/component/JsonAnimatedSprite.kt
+++ app/src/main/java/com/example/demoshemij/component/JsonAnimatedSprite.kt
@@ -3,12 +3,16 @@ package com.example.demoshemij.component
 import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
@@ -161,16 +165,13 @@ fun JsonAnimatedSprite(
         SpriteFlip1.RIGHT -> -1f
         else -> 1f
     }
-    
-    Box(
-        modifier = modifier.size(150.dp * (755 / 688f)),
-        contentAlignment = Alignment.TopEnd
-    ) {
+
         Image(
             bitmap = ImageBitmap.imageResource(id = imageRes),
             contentDescription = null,
             modifier = Modifier
-                .size(150.dp * (766 / 688f))
+                .size(150.dp * (755 / 688f))
+                .aspectRatio(755/688f)
                 .offset(
                     x = when {
                         spriteFlip == SpriteFlip1.TOP ||
@@ -209,10 +210,9 @@ fun JsonAnimatedSprite(
                             rotationZ = 0f
                         }
                     }
-                },
-            contentScale = ContentScale.Fit
+                }
+                .background(Color.Red),
         )
-    }
     
     // ✅ Log để debug - KEY RIÊNG
     LaunchedEffect(key1 = "debug_$characterName", key2 = currentFrameId, key3 = frameUrl) {
